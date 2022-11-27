8849d37bc93d9ff595aa5b1b08eca37621d668a5
Angel <angel42505@outlook.com>
2022-11-27 16:29:35 +0200

    Added the useQuery hook that gets the id of the note from the url

---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,15 @@
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+
+/**
+ *  Reads the ***query*** parameters
+ * @returns The note id which is read from the query parameters
+ */
+const useQuery = () => {
+  const { search } = useLocation();
+  const [query] = search.split('?id=')[1].split('&');
+
+  return useMemo(() => query, [query]);
+};
+
+export default useQuery;
--- src/pages/NotePage/NotePage.tsx
+++ src/pages/NotePage/NotePage.tsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Markdown from 'markdown-to-jsx';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import { HeaderInfo } from '../../models/header';
+import useQuery from '../../hooks/useQuery';
+import { useSelector } from 'react-redux';
+import { NotesSlice } from '../../models/store';
 
 const NotePage = () => {
+  const [num, setNum] = useState<number>(0);
   const navigate = useNavigate();
+  const noteId = useQuery();
+  const note = useSelector((state: NotesSlice) => state.notes.find(note => note.id === noteId));
+
+  console.log(note);
 
   const headerInfo: HeaderInfo = {
     heading: 'The_Title',
     buttons: [
       {
         text: 'Edit',
-        onClick: () => {},
+        onClick: () => {
+          setNum(prev => prev + 1);
+        },
       },
 
       {
